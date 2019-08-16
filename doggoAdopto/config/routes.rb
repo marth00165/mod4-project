e02d11807294694a16f75040257d8820f3b561ab
github username <github email address>
2019-08-16 11:04:54 -0700

    Removed all userpet files

--- doggoAdopto/config/routes.rb
+++ doggoAdopto/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :user_pets
   resources :pets
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- doggoAdopto/db/schema.rb
+++ doggoAdopto/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_215652) do
+ActiveRecord::Schema.define(version: 2019_08_14_215300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 2019_08_14_215652) do
     t.string "breed"
     t.string "desc"
     t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_pets", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "pet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
