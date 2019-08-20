436af6f665f5940023bda9c839b9034c5d3ced6c
github username <github email address>
2019-08-20 13:20:50 -0700

    setting state for myPets and fetching myPet information.

--- doggo-fronto-endo/src/Components/MyPets.js
+++ doggo-fronto-endo/src/Components/MyPets.js
@@ -2,17 +2,29 @@ import React, {Component} from 'react';
 import jwtDecode from 'jwt-decode';
 
 export default class Home extends Component {
+  state = {
+    myDogs: []
+  }
 
   componentDidMount = () => {
     let jwt = window.localStorage.getItem("jwt");
     let result = jwtDecode(jwt);
     let user_id = result.id
-    console.log(user_id)
+
+    fetch(`http://localhost:3000/users/${user_id}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.dogs[0].name)
+        this.setState({
+          myDogs: json.dogs
+        })
+      })
+
   }
 
   render(){
     return(
-      <h1>{this.props.userID}</h1>
+      <h1></h1>
 
     )
   }
